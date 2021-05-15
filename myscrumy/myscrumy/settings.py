69da5ae6f0ffc0b56999487509880df7322d2e5e
Idowu <emmanuelkila1@gmail.com>
2021-05-15 15:52:49 +0100

    Reusabe application

--- myscrumy/myscrumy/settings.py
+++ myscrumy/myscrumy/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-1+&_v=)sbv6yq#$eb)nyb-6aqb+so-ze&hd9u_v!0%lfkhzsiy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['18.219.132.180', '127.0.0.1']
+ALLOWED_HOSTS = ['3.133.111.89', '127.0.0.1']
 
 
 # Application definition
